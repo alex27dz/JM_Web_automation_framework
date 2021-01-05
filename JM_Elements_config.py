f5877f6f0ce1fdd25c72abf0a6832d4168bbb7b8
Alex Dezho <alex27dz@gmail.com>
2021-01-05 12:11:37 -0500

    Add files via upload

---
+++ JM_Elements_config.py
@@ -0,0 +1,17 @@
+# Configuration page for elements located in JM website pages
+
+# urls
+JMurl = "https://stage.jewelersmutual.com/"
+
+# text
+Header_Personal_Insurance_config = 'WHAT WILL IT COST ME?'
+validation_box_config = 'How the Big Names Compare to the Jewelry Insurance Experts'
+
+# Generic Elements
+Header_Personal_Insurance_elem = '//*[@id="block-jewelers-mutual-content"]/div/article/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div/a'
+validation_box_elem = '//*[@id="title-4366"]/h2'
+Personal_inurance_link = '//*[@id="block-jewelers-mutual-main-menu"]/ul/div/li[1]/ul/li[1]/a'
+
+
+
+
